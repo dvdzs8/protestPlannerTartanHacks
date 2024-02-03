d23dcee86957a0df8257e8b64b2d82fcaf1eba3b
goelaine <elainego@andrew.cmu.edu>
2024-02-03 16:09:33 -0500

    modified:   box.py 	modified:   frontpage.py

--- box.py
+++ box.py
@@ -8,67 +8,32 @@ col3 = '#fe4365' #header
 class Back():
 
     def __init__(self):
-
         #just the main empty background
         self.root = tk.Tk()
         self.root.geometry('1920x1080')
         self.root.title('Protest Organizor Toolkit')
-        self.mainframe = tk.Frame(self.root, background='white')
-        self.mainframe.pack(fill='both', expand=True)
+        self.canvas = tk.Canvas(self.root, bg=col2)
+        self.canvas.pack(expand=tk.YES, fill=tk.BOTH)
+        
+        def create_header():
+            banner = tk.Text(self.canvas, padx=10, pady=10, font = ("Oxygen", 30), bg=col3, relief="solid", borderwidth=2, wrap=tk.WORD, width=100, height=1)
+            banner.pack()
+            banner.insert(tk.END, "Title and organizers: ")
+        
+        def create_label(text1,x,y):
+            text = ttk.Label(self.canvas,text=text1, background=col1, font=("Oxygen", 20))
+            text.pack()
 
-        #david's bottom 2 squares
-
-        # self.objBox = tk.Canvas(self.root, bg=col2, width = 10, height = 10)
-        # self.objectives = tk.Label(self.objBox, text = "Hello World!", font=('Oxygen', 18))
-        # self.objBox.pack()
-        # self.objectives.pack()
-
-        canvas = tk.Canvas(self.mainframe, bg=col1)
-        canvas.create_rectangle(10,110, 625, 475, outline="black", width=2)
-        canvas.pack()
+        self.objBox = self.canvas.create_rectangle(10, 110, 625, 475, outline="black", width=2)
+        self.objTitle = tk.Label(self.canvas, text = "Objectives", font = ('Oxygen', 18))
+        self.objTitle.pack()
         
 
 
+        #david's bottom 2 square
         self.root.mainloop()
         return
 
 
 if __name__ == '__main__':
     Back()
-
-
-# from tkinter import * 
-# import tkinter as tk
-# from tkinter.ttk import *
-
-# class Box():
-
-#     #width adn height predetermined
-#     def __init__(self, root, title, x, y):
-
-#         self.canvas = Canvas(root)
-#         self.label = tk.Label(self.canvas, text = title, font = ('Helvetica',20))
-#         self.canvas.pack()
-
-#     def pack(self):
-#         self.canvas.pack()
-#         self.label
-
-# root = tk.Tk()
-# root.geometry("1920x1080")
-# root.title("My second GUI I guess")
-# label = tk.Label(root, text = "Hello World!", font=('Arial', 18)) #(part of root)
-# label.pack(padx = 20, pady = 20)
-# box = Box(root, "Test Box", 10,110)
-
-# root.mainloop()
-
-        
-# root.geometry("1920x1080")
-# root.title("My second GUI I guess")
-
-# label = tk.Label(root, text = "Hello World!", font=('Arial', 18)) #(part of root)
-# label.pack(padx = 20, pady = 20) # getting the label into the root using pack(padding)
-
-                 
-
--- frontpage.py
+++ frontpage.py
@@ -12,36 +12,36 @@ class Back():
         self.root = tk.Tk()
         self.root.geometry('1920x1080')
         self.root.title('Protest Organizor Toolkit')
-        self.mainframe = tk.Frame(self.root, background=col2)
-        self.mainframe.pack(fill='both', expand=True)
+        self.canvas = tk.Canvas(self.root, bg=col2)
+        self.canvas.pack(expand=tk.YES, fill=tk.BOTH)
         
         def create_header():
-            banner = tk.Text(self.mainframe, padx=10, pady=10, font = ("Oxygen", 30), bg=col3, relief="solid", borderwidth=2, wrap=tk.WORD, width=100, height=1)
+            banner = tk.Text(self.canvas, padx=10, pady=10, font = ("Oxygen", 30), bg=col3, relief="solid", borderwidth=2, wrap=tk.WORD, width=100, height=1)
             banner.pack()
             banner.insert(tk.END, "Title and organizers: ")
         
         def create_label(text1,x,y):
-            text = ttk.Label(self.mainframe,text=text1, background=col1, font=("Oxygen", 20))
+            text = ttk.Label(self.canvas,text=text1, background=col1, font=("Oxygen", 20))
             text.pack()
 
+
+        self.objBox = self.canvas.create_rectangle(10, 110, 625, 475, outline="black", width=2)
+
         #banner
-            
         self.banner = create_header()
         self.obj_label=create_label("Objectives",2,3)
-        #self.banner = tk.Text(font = ('Oxygen', 16), bg="blue", pady= 0, borderwidth=2, relief="solid", width=1920, height=100)
-        #self.banner.pack
+
+        
+        self.side = self.canvas.create_rectangle(1280, 80, 640, 800, outline="black", width=10)
+        self.side.pack()
 
         
 
-        
 
 
 
 
-        #david's bottom 2 squares
-
-        contactBox = tk.Text()
-
+        #david's bottom 2 square
         self.root.mainloop()
         return
 
