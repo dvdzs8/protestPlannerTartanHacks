c0fc3bc9938965054fd31e8bac773ae91908837a
goelaine <elainego@andrew.cmu.edu>
2024-02-03 15:23:21 -0500

    new file:   box.py 	modified:   frontpage.py 	deleted:    practice/davidPractice

--- box.py
+++ box.py
@@ -43,3 +43,40 @@ class Back():
 
 if __name__ == '__main__':
     Back()
+
+
+# from tkinter import * 
+# import tkinter as tk
+# from tkinter.ttk import *
+
+# class Box():
+
+#     #width adn height predetermined
+#     def __init__(self, root, title, x, y):
+
+#         self.canvas = Canvas(root)
+#         self.label = tk.Label(self.canvas, text = title, font = ('Helvetica',20))
+#         self.canvas.pack()
+
+#     def pack(self):
+#         self.canvas.pack()
+#         self.label
+
+# root = tk.Tk()
+# root.geometry("1920x1080")
+# root.title("My second GUI I guess")
+# label = tk.Label(root, text = "Hello World!", font=('Arial', 18)) #(part of root)
+# label.pack(padx = 20, pady = 20)
+# box = Box(root, "Test Box", 10,110)
+
+# root.mainloop()
+
+        
+# root.geometry("1920x1080")
+# root.title("My second GUI I guess")
+
+# label = tk.Label(root, text = "Hello World!", font=('Arial', 18)) #(part of root)
+# label.pack(padx = 20, pady = 20) # getting the label into the root using pack(padding)
+
+                 
+
