da23addb6b6929a9461159556c521bf20bf36031
goelaine <elainego@andrew.cmu.edu>
2024-02-03 13:15:43 -0500

    modified:   davidPractice 	modified:   elainepractice.py

--- elainepractice.py
+++ elainepractice.py
@@ -1,2 +1,19 @@
 import tkinter as tk
 from tkinter import ttk
+
+class App():
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry('350x200')
+        self.root.title('Text App')
+        self.mainframe = tk.Frame(self.root, background='yellow')
+        self.mainframe.pack(fill='both', expand=True)
+
+        self.text = ttk.Label(self.mainframe,text='Welcome to your protestor toolkit', background='white', font=("Times New Roman", 30))
+        self.text.grid(row=0, column=0)
+        self.root.mainloop()
+        return
+    
+
+if __name__ == '__main__':
+    App()
